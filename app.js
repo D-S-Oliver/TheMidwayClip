e98cccecae3a90bd9837b87fd9df21bd92df9570
Oliver (Bruno) <ukyio.sun@gmail.com>
2021-12-08 17:47:13 -0300

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const app = require("express")(); 
 const PORT = process.env.PORT || 3000; 
 //app.use('/', express.static(__dirname + '/public')); 
-app.get("", (req, res) => { res.send("Hello World"); 
+app.get("", (req, res) => { res.send("EI... VOCÊ AÍ!\n ME DÁ UM DINHEIRO AÍ\n ME DÁ UM DINHEIRO AÍ\n ME DÁ UM DINHEIRO AÍ!\n"); 
 }); 
 app.listen(PORT, () => { console.log(`App up at port ${PORT}`); 
 }
